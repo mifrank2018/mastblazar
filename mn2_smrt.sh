40905a97e367a00fc077ae2e0348e641395693f4
mifrank2018 <yopach0@gmail.com>
2018-05-19 16:01:28 -0500

    n

--- mn2_smrt.sh
+++ mn2_smrt.sh
@@ -1,73 +1,165 @@
 #!/bin/bash
 
 TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='smrt.conf'
-CONFIGFOLDER='/root/.smrt'
-COIN_DAEMON='smrtd'
-COIN_CLI='smrt-cli'
-COIN_PATH='/usr/local/bin/'
+CONFIG_FILE="smrt.conf"
+BINARY_FILE="/usr/local/bin/smrtd"
+CROP_REPO="https://github.com/smrt-crypto/smrt.git"
 COIN_TGZ='https://github.com/zoldur/Smrt/releases/download/v1.1.0.5/smrt.tar.gz'
-COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='Smrt'
-COIN_PORT=52310
-RPC_PORT=52312
-
-NODEIP=$(curl -s4 icanhazip.com)
-
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 
-function download_node() {
-  echo -e "Prepare to download $COIN_NAME binaries"
-  cd $TMP_FOLDER
-  wget -q $COIN_TGZ
-  tar xvzf $COIN_ZIP -C $COIN_PATH >/dev/null 2>&1
-  compile_error
-  chmod +x $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI
-  cd - >/dev/null 2>&1
-  rm -r $TMP_FOLDER >/dev/null 2>&1
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $@. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+
+function checks() {
+if [[ $(lsb_release -d) != *16.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+
+if [ -n "$(pidof cropcoind)" ]; then
+  echo -e "${GREEN}\c"
+  read -e -p "Cropcoind is already running. Do you want to add another MN? [Y/N]" NEW_CROP
+  echo -e "{NC}"
+  clear
+else
+  NEW_CROP="new"
+fi
+}
+
+function prepare_system() {
+
+echo -e "Prepare the system to install Cropcoin master node."
+apt-get update >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+apt install -y software-properties-common >/dev/null 2>&1
+echo -e "${GREEN}Adding bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+echo -e "Installing required packages, it may take some time to finish.${NC}"
+apt-get update >/dev/null 2>&1
+apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget pwgen curl libdb4.8-dev bsdmainutils \
+libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw pwgen
+clear
+if [ "$?" -gt "0" ];
+  then
+    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+    echo "apt-get update"
+    echo "apt -y install software-properties-common"
+    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+    echo "apt-get update"
+    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw"
+ exit 1
+fi
+
+clear
+echo -e "Checking if swap space is needed."
+PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
+SWAP=$(swapon -s)
+if [[ "$PHYMEM" -lt "2" && -z "$SWAP" ]];
+  then
+    echo -e "${GREEN}Server is running with less than 2G of RAM, creating 2G swap file.${NC}"
+    dd if=/dev/zero of=/swapfile bs=1024 count=2M
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon -a /swapfile
+else
+  echo -e "${GREEN}The server running with at least 2G of RAM, or SWAP exists.${NC}"
+fi
+clear
+}
+
+function deploy_binaries() {
+  cd $TMP
+  wget -q $COIN_TGZ >/dev/null 2>&1
+  gunzip cropcoind.gz >/dev/null 2>&1
+  chmod +x cropcoind >/dev/null 2>&1
+  cp cropcoind /usr/local/bin/ >/dev/null 2>&1
+}
+
+function ask_permission() {
+ echo -e "${RED}I trust zoldur and want to use binaries compiled on his server.${NC}."
+ echo -e "Please type ${RED}YES${NC} if you want to use precompiled binaries, or type anything else to compile them on your server"
+ read -e ZOLDUR
+}
+
+function compile_cropcoin() {
+  echo -e "Clone git repo and compile it. This may take some time. Press a key to continue."
+  read -n 1 -s -r -p ""
+
+  git clone $CROP_REPO $TMP_FOLDER
+  cd $TMP_FOLDER/src
+  mkdir obj/support
+  mkdir obj/crypto
+  make -f makefile.unix
+  compile_error cropcoin
+  cp -a cropcoind $BINARY_FILE
   clear
 }
 
-function configure_systemd() {
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
+function enable_firewall() {
+  echo -e "Installing and setting up firewall to allow incomning access on port ${GREEN}$CROPCOINPORT${NC}"
+  ufw allow $CROPCOINPORT/tcp comment "Cropcoin MN port" >/dev/null
+  ufw allow $[CROPCOINPORT+1]/tcp comment "Cropcoin RPC port" >/dev/null
+  ufw allow ssh >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "y" | ufw enable >/dev/null 2>&1
+}
+
+function systemd_cropcoin() {
+  cat << EOF > /etc/systemd/system/$CROPCOINUSER.service
 [Unit]
-Description=$COIN_NAME service
+Description=Cropcoin service
 After=network.target
 [Service]
-User=root
-Group=root
 Type=forking
-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
-ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
-ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
+User=$CROPCOINUSER
+Group=$CROPCOINUSER
+WorkingDirectory=$CROPCOINHOME
+ExecStart=$BINARY_FILE -daemon
+ExecStop=$BINARY_FILE stop
 Restart=always
 PrivateTmp=true
 TimeoutStopSec=60s
 TimeoutStartSec=10s
 StartLimitInterval=120s
 StartLimitBurst=5
+  
 [Install]
 WantedBy=multi-user.target
 EOF
 
   systemctl daemon-reload
   sleep 3
-  systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+  systemctl start $CROPCOINUSER.service
+  systemctl enable $CROPCOINUSER.service >/dev/null 2>&1
 
-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
-    echo -e "less /var/log/syslog${NC}"
+  if [[ -z $(pidof cropcoind) ]]; then
+    echo -e "${RED}Cropcoind is not running${NC}, please investigate. You should start by running the following commands as root:"
+    echo "systemctl start $CROPCOINUSER.service"
+    echo "systemctl status $CROPCOINUSER.service"
+    echo "less /var/log/syslog"
     exit 1
   fi
 }
-
 
 function ask_port() {
 DEFAULTCROPCOINPORT=52310
@@ -110,178 +202,71 @@ function check_port() {
   done
 }
 
-
 function create_config() {
-  mkdir $CONFIGFOLDER >/dev/null 2>&1
   RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
   RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
-  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+  cat << EOF > $CROPCOINFOLDER/$CONFIG_FILE
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
 rpcallowip=127.0.0.1
+rpcport=$[CROPCOINPORT+1]
 listen=1
 server=1
 daemon=1
-port=$COIN_PORT
+port=$CROPCOINPORT
 EOF
 }
 
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-  read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+  echo -e "Enter your ${RED}Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  read -e CROPCOINKEY
+  if [[ -z "$CROPCOINKEY" ]]; then
+  sudo -u $CROPCOINUSER /usr/local/bin/cropcoind -conf=$CROPCOINFOLDER/$CONFIG_FILE -datadir=$CROPCOINFOLDER
+  sleep 5
+  if [ -z "$(pidof cropcoind)" ]; then
+   echo -e "${RED}Cropcoind server couldn't start. Check /var/log/syslog for errors.{$NC}"
    exit 1
   fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
+  CROPCOINKEY=$(sudo -u $CROPCOINUSER $BINARY_FILE -conf=$CROPCOINFOLDER/$CONFIG_FILE -datadir=$CROPCOINFOLDER masternode genkey)
+  sudo -u $CROPCOINUSER $BINARY_FILE -conf=$CROPCOINFOLDER/$CONFIG_FILE -datadir=$CROPCOINFOLDER stop
 fi
-clear
 }
 
 function update_config() {
-  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
-  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
-logintimestamps=1
+  sed -i 's/daemon=1/daemon=0/' $CROPCOINFOLDER/$CONFIG_FILE
+  NODEIP=$(curl -s4 icanhazip.com)
+  cat << EOF >> $CROPCOINFOLDER/$CONFIG_FILE
+logtimestamps=1
 maxconnections=256
-#bind=$NODEIP
 masternode=1
-externalip=$NODEIP:$COIN_PORT
-masternodeprivkey=$COINKEY
+masternodeaddr=$NODEIP:$CROPCOINPORT
+masternodeprivkey=$CROPCOINKEY
 EOF
+  chown -R $CROPCOINUSER: $CROPCOINFOLDER >/dev/null
 }
-
-
-function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
-  ufw allow ssh comment "SSH" >/dev/null 2>&1
-  ufw limit ssh/tcp >/dev/null 2>&1
-  ufw default allow outgoing >/dev/null 2>&1
-  echo "y" | ufw enable >/dev/null 2>&1
-}
-
-
-
-function get_ip() {
-  declare -a NODE_IPS
-  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
-  do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
-  done
-
-  if [ ${#NODE_IPS[@]} -gt 1 ]
-    then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
-      INDEX=0
-      for ip in "${NODE_IPS[@]}"
-      do
-        echo ${INDEX} $ip
-        let INDEX=${INDEX}+1
-      done
-      read -e choose_ip
-      NODEIP=${NODE_IPS[$choose_ip]}
-  else
-    NODEIP=${NODE_IPS[0]}
-  fi
-}
-
-
-function compile_error() {
-if [ "$?" -gt "0" ];
- then
-  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
-  exit 1
-fi
-}
-
-
-function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-
-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
-  echo -e "${GREEN}\c"
-  read -e -p "Smrtd is already running. Do you want to add another MN? [Y/N]" NEW_CROP
-  echo -e "{NC}"
-  clear
-else
-  NEW_CROP="new"
-fi
-}
-
-function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ libzmq5 >/dev/null 2>&1
-if [ "$?" -gt "0" ];
-  then
-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-    echo "apt-get update"
-    echo "apt -y install software-properties-common"
-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
-    echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5"
- exit 1
-fi
-
-clear
-}
-
 
 function important_information() {
  echo
  echo -e "================================================================================================================================"
- echo -e "$COIN_NAME Masternode is up and running listening on port ${RED}$COIN_PORT${NC}."
- echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
- echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
- echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
- echo -e "VPS_IP:PORT ${RED}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE PRIVATEKEY is: ${RED}$COINKEY${NC}"
- echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
- echo -e "Use ${RED}$COIN_CLI masternode status${NC} to check your Masternode."
+ echo -e "Cropcoin Masternode is up and running as user ${GREEN}$CROPCOINUSER${NC} and it is listening on port ${GREEN}$CROPCOINPORT${NC}."
+ echo -e "${GREEN}$CROPCOINUSER${NC} password is ${RED}$USERPASS${NC}"
+ echo -e "Configuration file is: ${RED}$CROPCOINFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Start: ${RED}systemctl start $CROPCOINUSER.service${NC}"
+ echo -e "Stop: ${RED}systemctl stop $CROPCOINUSER.service${NC}"
+ echo -e "VPS_IP:PORT ${RED}$NODEIP:$CROPCOINPORT${NC}"
+ echo -e "MASTERNODE PRIVATEKEY is: ${RED}$CROPCOINKEY${NC}"
  echo -e "================================================================================================================================"
 }
 
 function setup_node() {
-  
   ask_user
-  check_port  
-  get_ip
+  check_port
   create_config
   create_key
   update_config
   enable_firewall
+  systemd_cropcoin
   important_information
-  configure_systemd
 }
 
 
@@ -293,7 +278,15 @@ if [[ ("$NEW_CROP" == "y" || "$NEW_CROP" == "Y") ]]; then
   setup_node
   exit 0
 elif [[ "$NEW_CROP" == "new" ]]; then
-prepare_system
-download_node
-setup_node
+  prepare_system
+  ask_permission
+  if [[ "$ZOLDUR" == "YES" ]]; then
+    deploy_binaries
+  else
+    compile_cropcoin
+  fi
+  setup_node
+else
+  echo -e "${GREEN}Cropcoind already running.${NC}"
+  exit 0
 fi
